c369d78a8618049c95268ceda8bb4403a61f1ab3
andrewgobert <108718840+andrewgobert@users.noreply.github.com>
2023-02-28 08:50:51 -0600

    Add files via upload

---
+++ Hospital/src/main/java/com/mycompany/hospital/dao/RoomDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.hospital.dao;
+
+import com.mycompany.hospital.binary.Room;
+
+/**
+ *
+ * @author Andrew Gobert
+ */
+public interface RoomDAO extends AbstractDAO<Room> {
+    
+}
