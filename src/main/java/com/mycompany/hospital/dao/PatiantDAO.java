fff70d4e9abf282c823b50b0dca92a0a26c2554c
andrewgobert <108718840+andrewgobert@users.noreply.github.com>
2023-03-07 08:00:23 -0600

    Add files via upload

---
+++ src/main/java/com/mycompany/hospital/dao/PatiantDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.hospital.dao;
+
+import com.mycompany.hospital.binary.Patiant;
+
+/**
+ *
+ * @author Andrew Gobert
+ */
+public interface PatiantDAO extends AbstractDAO<Patiant> {
+    
+}
