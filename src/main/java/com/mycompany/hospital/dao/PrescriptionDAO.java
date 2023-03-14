a1414ecf0ea638966c22848e831b8e39452e5434
andrewgobert <108718840+andrewgobert@users.noreply.github.com>
2023-03-14 08:17:54 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/hospital/dao/PrescriptionDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.hospital.dao;
+
+import com.mycompany.hospital.binary.Prescription;
+
+/**
+ *
+ * @author Andrew Gobert
+ */
+public interface PrescriptionDAO extends AbstractDAO<Prescription>  {
+    
+}
